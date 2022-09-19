4fa51d81eddddce86b63a1d4727669a9755c0adc
nariaki3551 <nariaki3551@gmail.com>
2022-09-19 11:31:05 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ppulp",
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.1.0",
+    license="MIT",
+    install_requires=[
+        "numpy",
+        "sympy",
+        "matplotlib==3.5.2",
+        "pulp",
+        "optuna==2.10.1",
+        "hyperopt",
+        "cvxopt",
+        "amplify",
+        "pytest",
+        "scipy",
+        "timeout_decorator",
+    ],
+    author="nariaki tateiwa",
+    author_email="nariaki3551@gmail.com",
+    # url="https://ppulp.readthedocs.io/en/latest/index.html",
+    description="An extension of PuLP, a linear programming problem modeling tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="optimization LP linear programming modeling",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
